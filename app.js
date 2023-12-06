4a46d07cc1cd679aa866055dbfc28243e1423b0a
preeentrega 2 <mateoobenitez99@gmail.com>
2023-12-06 14:41:13 -0300

    entrega 3

--- app.js
+++ app.js
@@ -1,20 +1,51 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const btnMostrarResultados = document.getElementById("btnMostrarResultados");
+    const resultadosContainer = document.getElementById("resultados");
 
-    const correos = prompt("Ingrese correos electrónicos separados por comas para recibir información adicional:").split(',');
-    const telefonos = prompt("Ingrese números telefónicos separados por comas para recibir información adicional:").split(',');
 
-    const datosContacto = {
-        correos: correos,
-        telefonos: telefonos
-    };
+    function enviarDatosContacto(datosContacto) {
+        console.log("Datos de contacto enviados:");
+        console.log("Correo electrónico:", datosContacto.correo);
+        console.log("Número telefónico:", datosContacto.telefono);
 
-    enviarDatosContacto(datosContacto);
+
+        const datosContactoJSON = JSON.stringify(datosContacto);
+        localStorage.setItem('datosContacto', datosContactoJSON);
+
+        console.log("¡Gracias por enviar tus datos!");
+    }
+
+
+    function mostrarResultados() {
+
+        const datosContactoJSON = localStorage.getItem('datosContacto');
+        if (datosContactoJSON) {
+            const datosContacto = JSON.parse(datosContactoJSON);
+
+            resultadosContainer.innerHTML = `
+                <h3>Resultados:</h3>
+                <p>Correo electrónico: ${datosContacto.correo}</p>
+                <p>Número telefónico: ${datosContacto.telefono}</p>
+            `;
+        } else {
+            resultadosContainer.innerHTML = `<p>No hay resultados almacenados.</p>`;
+        }
+    }
+
+
+    btnMostrarResultados.addEventListener("click", function () {
+        mostrarResultados();
+    });
+
+    document.getElementById("btnEnviarDatos").addEventListener("click", function () {
+        const correo = prompt("Ingrese su correo electrónico:");
+        const telefono = prompt("Ingrese su número telefónico:");
+
+        const datosContacto = {
+            correo: correo,
+            telefono: telefono
+        };
+
+        enviarDatosContacto(datosContacto);
+    });
 });
-
-function enviarDatosContacto(datosContacto) {
-
-    console.log("Datos de contacto enviados para recibir información adicional:");
-    console.log("Correos electrónicos:", datosContacto.correos);
-    console.log("Números telefónicos:", datosContacto.telefonos);
-    console.log("¡Gracias por enviar tus datos!");
-}
